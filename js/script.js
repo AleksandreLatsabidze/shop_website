60a09697c6e47e6907f50a5dd980779d92b9b028
aleksandrelcb <aleksandrelatsabidze1@gmail.com>
2023-08-11 21:15:48 +0400

    finished the fourth page

--- js/script.js
+++ js/script.js
@@ -1,25 +1,7 @@
-// document.querySelectorAll(".catitem-h2").forEach((element) =>
-//   element.addEventListener("click", (event) => {
-//     element.nextElementSibling.classList.toggle("catshow-hide");
-//   })
-// );
-
-// document.getElementById("my-select").addEventListener("change", function () {
-//   console.log("You selected: ", this.value);
-//   window.location.href = this.value;
-// });
-
-// const element = document.getElementById("sidebar-blur");
-// const sidebar = document.getElementById("sidebar");
-// const closesidebar = document.getElementById("close");
-// document.getElementById("sidenavbar").addEventListener("click", function () {
-//   element.classList.add("showsidebarblur");
-//   sidebar.classList.add("showsidebar");
-// });
-// document.getElementById("close").addEventListener("click", function () {
-//   element.classList.remove("showsidebarblur");
-//   sidebar.classList.remove("showsidebar");
-// });
+document.getElementById("my-select").addEventListener("change", function () {
+  console.log("You selected: ", this.value);
+  window.location.href = this.value;
+});
 
 const boxes = document.getElementsByClassName("iphone-sale");
 
@@ -38,4 +20,22 @@ document.getElementById("filter-two").addEventListener("click", function () {
   for (const box of boxes) {
     box.classList.remove("filter-one");
   }
+});
+
+document.querySelectorAll(".catitem-h2").forEach((element) =>
+  element.addEventListener("click", (event) => {
+    element.nextElementSibling.classList.toggle("catshow-hide");
+  })
+);
+
+const element = document.getElementById("sidebar-blur");
+const sidebar = document.getElementById("sidebar");
+const closesidebar = document.getElementById("close");
+document.getElementById("sidenavbar").addEventListener("click", function () {
+  element.classList.add("showsidebarblur");
+  sidebar.classList.add("showsidebar");
+});
+document.getElementById("close").addEventListener("click", function () {
+  element.classList.remove("showsidebarblur");
+  sidebar.classList.remove("showsidebar");
 });
